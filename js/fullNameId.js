7b81a3986e1d7bd691c6585790af5facc7beaa4f
Judjin-debug <yauheniN27@protonmail.com>
2023-05-20 19:53:56 +0300

    docs: add fullNameId.js

---
+++ js/fullNameId.js
@@ -0,0 +1,26 @@
+let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+let petya = { name: "Петя", surname: "Иванов", id: 2 };
+let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+let users = [ vasya, petya, masha ];
+
+
+function mappedUser(user) {
+    return {
+        fullName : user.name + ' ' + user.surname,
+        id : user.id
+    };
+};
+
+let usersMapped = users.map(mappedUser);
+
+/*
+usersMapped = [
+  { fullName: "Вася Пупкин", id: 1 },
+  { fullName: "Петя Иванов", id: 2 },
+  { fullName: "Маша Петрова", id: 3 }
+]
+*/
+
+console.log( usersMapped[0].id ) // 1
+console.log( usersMapped[0].fullName ) // Вася Пупкин
