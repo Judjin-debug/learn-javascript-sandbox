3e14f513fdae2153d0d352e5bb22a9d13a222599
Judjin-debug <yauheniN27@protonmail.com>
2023-05-29 20:18:27 +0300

    fix: change set to map in aclean func

--- js/aclean.js
+++ js/aclean.js
@@ -1,23 +1,43 @@
 function aclean (array) {
 
 
-    const eqSet = (xs, ys) =>
-        xs.size === ys.size &&
-        [...xs].every((x) => ys.has(x));
+    function strToMap (str) {
+        let map = new Map();
+        let lcStr = str.toLowerCase();
+        for (char of lcStr) {
+            if (!map.has(char)) {
+                map.set(char, 1);
+            } else {
+                let val = map.get(char);
+                map.set(char, ++val);
+            };
+        };
+        return map;
+    };
 
-    let aset = array.map(v => new Set(v.toLowerCase().split('')));
+
+    function mapEq (xm, ym) {
+        if (!(xm.size === ym.size)) return false;
+        for (let key of xm.keys()) {
+            if (!(ym.has(key) && xm.get(key) === ym.get(key))) return false;
+        }
+        return true;
+    };
+
+    let amap = array.map(v => strToMap(v));
     let alen = array.length;
     let ares = array.slice();
     for (let i = 0; i < alen - 1; i++) {
         for (let j = alen - 1; j > i; j--) {
-            if (eqSet(aset[i], aset[j])) {
+            if (mapEq(amap[i], amap[j])) {
                 ares.splice(j, 1);
-                aset.splice(j, 1);
+                amap.splice(j, 1);
                 alen--;
             };
         };
     };
     return ares;
+
 };
 
 let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
