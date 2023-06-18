73efc8d184ba86cf10569882d546c0b2a3875163
Judjin-debug <yauheniN27gmail.com>
2023-06-18 21:21:52 +0300

    docs: add printList

---
+++ js/printList.js
@@ -0,0 +1,33 @@
+function printList(list) {
+    console.log(list.value);
+    if (list.next !== null) return printList(list.next);
+};
+
+function printListLoop(list) {
+    console.log(list.value);
+    while (list.next !== null) {
+        list = list.next;
+        console.log(list.value);
+    }
+}
+
+let list = {
+    value: 1,
+    next: {
+        value: 2,
+        next: {
+            value: 3,
+            next: {
+                value: 4,
+                next: null
+            }
+        }
+    }       
+};
+
+console.log(`Recursion:`);
+printList(list);
+
+console.log(`Loop:`);
+printListLoop(list);
+
